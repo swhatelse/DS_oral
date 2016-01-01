9277faa917ef887dbe2e3e59228d6d17874fc52e
swhatelse <stevenwhatelse@hotmail.fr>
2016-01-01 10:39:58 +0100

    U

--- thesecretlivesofdata/raft/scripts/frames/init.js
+++ thesecretlivesofdata/raft/scripts/frames/init.js
@@ -3,10 +3,11 @@
 /*jslint browser: true, nomen: true*/
 /*global define*/
 
-define(["./playground", "./title", "./election2", "./replication"],
-    function (playground, title, election2, replication) {
+define(["./playground", "./title", "./election2", "./replication", "./replication2"],
+    function (playground, title, election2, replication, replication2) {
         return function (player) {
             player.frame("election2", "Leader Election", election2);
+            player.frame("replication2", "Log Replication", replication2);
             player.frame("replication", "Log Replication", replication);
         };
     });
